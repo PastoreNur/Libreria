bb88b48ef3e6ecfe93c537861654250a1c764d46
PastoreNur <julio.pastore.jp@gmail.com>
2016-09-27 01:32:16 -0600

    Diseño
    Cambio de colores temporales y nuevo panel central para mantener objetos
    aliniados

--- FrmCatalogo.Designer.cs
+++ FrmCatalogo.Designer.cs
@@ -29,18 +29,16 @@
         private void InitializeComponent()
         {
             this.panelencabezado = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
-
-            this.panelmenu = new System.Windows.Forms.Panel();
-
             this.button2 = new System.Windows.Forms.Button();
-
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelmenu = new System.Windows.Forms.Panel();
+            this.PanelCentral = new System.Windows.Forms.Panel();
             this.panelencabezado.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelencabezado
             // 
+            this.panelencabezado.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
             this.panelencabezado.Controls.Add(this.button2);
             this.panelencabezado.Controls.Add(this.button1);
             this.panelencabezado.Dock = System.Windows.Forms.DockStyle.Top;
@@ -49,41 +47,6 @@
             this.panelencabezado.Size = new System.Drawing.Size(894, 42);
             this.panelencabezado.TabIndex = 2;
             // 
-            // button1
-            // 
-
-            this.button1.Location = new System.Drawing.Point(446, 12);
-
-            this.button1.Location = new System.Drawing.Point(73, 13);
-
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Clic";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // vScrollBar1
-            // 
-            this.vScrollBar1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.vScrollBar1.LargeChange = 101;
-            this.vScrollBar1.Location = new System.Drawing.Point(877, 42);
-            this.vScrollBar1.Name = "vScrollBar1";
-            this.vScrollBar1.Size = new System.Drawing.Size(17, 505);
-            this.vScrollBar1.TabIndex = 3;
-            // 
-
-            // panelmenu
-            // 
-            this.panelmenu.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.panelmenu.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panelmenu.Location = new System.Drawing.Point(0, 42);
-            this.panelmenu.Name = "panelmenu";
-            this.panelmenu.Size = new System.Drawing.Size(134, 505);
-            this.panelmenu.TabIndex = 5;
-            this.panelmenu.MouseLeave += new System.EventHandler(this.panelmenu_MouseLeave);
-            this.panelmenu.MouseHover += new System.EventHandler(this.panelmenu_MouseHover);
-
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(584, 12);
@@ -93,15 +56,45 @@
             this.button2.Text = "Salir Temporal";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
-
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(73, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Clic";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panelmenu
+            // 
+            this.panelmenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(126)))), ((int)(((byte)(34)))));
+            this.panelmenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelmenu.Location = new System.Drawing.Point(0, 42);
+            this.panelmenu.Name = "panelmenu";
+            this.panelmenu.Size = new System.Drawing.Size(134, 505);
+            this.panelmenu.TabIndex = 5;
+            this.panelmenu.MouseLeave += new System.EventHandler(this.panelmenu_MouseLeave);
+            this.panelmenu.MouseHover += new System.EventHandler(this.panelmenu_MouseHover);
+            // 
+            // PanelCentral
+            // 
+            this.PanelCentral.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(240)))), ((int)(((byte)(241)))));
+            this.PanelCentral.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PanelCentral.Location = new System.Drawing.Point(134, 42);
+            this.PanelCentral.Name = "PanelCentral";
+            this.PanelCentral.Size = new System.Drawing.Size(760, 505);
+            this.PanelCentral.TabIndex = 7;
             // 
             // FrmCatalogo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(195)))), ((int)(((byte)(199)))));
             this.ClientSize = new System.Drawing.Size(894, 547);
+            this.Controls.Add(this.PanelCentral);
             this.Controls.Add(this.panelmenu);
-            this.Controls.Add(this.vScrollBar1);
             this.Controls.Add(this.panelencabezado);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.IsMdiContainer = true;
@@ -116,12 +109,12 @@
         #endregion
 
         private System.Windows.Forms.Panel panelencabezado;
-        private System.Windows.Forms.VScrollBar vScrollBar1;
         private System.Windows.Forms.Button button1;
 
         private System.Windows.Forms.Panel panelmenu;
 
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel PanelCentral;
 
     }
 }
