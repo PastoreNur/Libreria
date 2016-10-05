17cb0d5c520c6fc4ed1a07ad0cf688f7b818775b
c3rberuss <aaamaya98@gmail.com>
2016-10-04 23:55:05 -0600

    ...

--- Principal.Designer.cs
+++ Principal.Designer.cs
@@ -101,7 +101,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(353, 15);
+            this.label2.Location = new System.Drawing.Point(401, 14);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(109, 13);
             this.label2.TabIndex = 4;
