cc57889053e7f3bf811add022f0e061eafca0c80
c3rberuss <aaamaya98@gmail.com>
2016-10-04 23:27:41 -0600

    :v:

--- Principal.Designer.cs
+++ Principal.Designer.cs
@@ -30,24 +30,24 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Principal));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.BtnCarrito = new System.Windows.Forms.PictureBox();
-            this.BtnUser = new System.Windows.Forms.PictureBox();
             this.BtnMinimizar = new System.Windows.Forms.PictureBox();
             this.BtnCerrar = new System.Windows.Forms.PictureBox();
-            this.LblUsuario = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LblUsuario = new System.Windows.Forms.Label();
+            this.BtnUser = new System.Windows.Forms.PictureBox();
+            this.BtnCarrito = new System.Windows.Forms.PictureBox();
             this.loginBar = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.BtnIngresarBar = new System.Windows.Forms.Button();
+            this.txtPAsswordBar = new System.Windows.Forms.TextBox();
+            this.txtUsuarioBar = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.BtnCarrito)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.BtnUser)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.BtnMinimizar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.BtnCerrar)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnUser)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnCarrito)).BeginInit();
             this.loginBar.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -61,41 +61,6 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1000, 30);
             this.panel1.TabIndex = 1;
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.panel2.Controls.Add(this.label2);
-            this.panel2.Controls.Add(this.label1);
-            this.panel2.Controls.Add(this.LblUsuario);
-            this.panel2.Controls.Add(this.BtnUser);
-            this.panel2.Controls.Add(this.BtnCarrito);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 30);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1000, 40);
-            this.panel2.TabIndex = 2;
-            // 
-            // BtnCarrito
-            // 
-            this.BtnCarrito.Image = ((System.Drawing.Image)(resources.GetObject("BtnCarrito.Image")));
-            this.BtnCarrito.Location = new System.Drawing.Point(960, 5);
-            this.BtnCarrito.Name = "BtnCarrito";
-            this.BtnCarrito.Size = new System.Drawing.Size(30, 30);
-            this.BtnCarrito.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.BtnCarrito.TabIndex = 0;
-            this.BtnCarrito.TabStop = false;
-            // 
-            // BtnUser
-            // 
-            this.BtnUser.Image = ((System.Drawing.Image)(resources.GetObject("BtnUser.Image")));
-            this.BtnUser.Location = new System.Drawing.Point(838, 5);
-            this.BtnUser.Name = "BtnUser";
-            this.BtnUser.Size = new System.Drawing.Size(30, 30);
-            this.BtnUser.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.BtnUser.TabIndex = 1;
-            this.BtnUser.TabStop = false;
-            this.BtnUser.Click += new System.EventHandler(this.BtnUser_Click);
             // 
             // BtnMinimizar
             // 
@@ -119,23 +84,19 @@
             this.BtnCerrar.TabStop = false;
             this.BtnCerrar.Click += new System.EventHandler(this.BtnCerrar_Click);
             // 
-            // LblUsuario
+            // panel2
             // 
-            this.LblUsuario.AutoSize = true;
-            this.LblUsuario.Location = new System.Drawing.Point(871, 22);
-            this.LblUsuario.Name = "LblUsuario";
-            this.LblUsuario.Size = new System.Drawing.Size(83, 13);
-            this.LblUsuario.TabIndex = 2;
-            this.LblUsuario.Text = "Nombre Usuario";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(871, 3);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Entrar";
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.LblUsuario);
+            this.panel2.Controls.Add(this.BtnUser);
+            this.panel2.Controls.Add(this.BtnCarrito);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 30);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1000, 40);
+            this.panel2.TabIndex = 2;
             // 
             // label2
             // 
@@ -146,54 +107,93 @@
             this.label2.TabIndex = 4;
             this.label2.Text = "Nombre de Categoría";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(871, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Entrar";
+            // 
+            // LblUsuario
+            // 
+            this.LblUsuario.AutoSize = true;
+            this.LblUsuario.Location = new System.Drawing.Point(871, 22);
+            this.LblUsuario.Name = "LblUsuario";
+            this.LblUsuario.Size = new System.Drawing.Size(83, 13);
+            this.LblUsuario.TabIndex = 2;
+            this.LblUsuario.Text = "Nombre Usuario";
+            // 
+            // BtnUser
+            // 
+            this.BtnUser.Image = ((System.Drawing.Image)(resources.GetObject("BtnUser.Image")));
+            this.BtnUser.Location = new System.Drawing.Point(838, 5);
+            this.BtnUser.Name = "BtnUser";
+            this.BtnUser.Size = new System.Drawing.Size(30, 30);
+            this.BtnUser.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BtnUser.TabIndex = 1;
+            this.BtnUser.TabStop = false;
+            this.BtnUser.Click += new System.EventHandler(this.BtnUser_Click);
+            // 
+            // BtnCarrito
+            // 
+            this.BtnCarrito.Image = ((System.Drawing.Image)(resources.GetObject("BtnCarrito.Image")));
+            this.BtnCarrito.Location = new System.Drawing.Point(960, 5);
+            this.BtnCarrito.Name = "BtnCarrito";
+            this.BtnCarrito.Size = new System.Drawing.Size(30, 30);
+            this.BtnCarrito.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BtnCarrito.TabIndex = 0;
+            this.BtnCarrito.TabStop = false;
+            // 
             // loginBar
             // 
             this.loginBar.BackColor = System.Drawing.Color.Transparent;
             this.loginBar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("loginBar.BackgroundImage")));
             this.loginBar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.loginBar.Controls.Add(this.button1);
-            this.loginBar.Controls.Add(this.textBox2);
-            this.loginBar.Controls.Add(this.textBox1);
+            this.loginBar.Controls.Add(this.BtnIngresarBar);
+            this.loginBar.Controls.Add(this.txtPAsswordBar);
+            this.loginBar.Controls.Add(this.txtUsuarioBar);
             this.loginBar.Enabled = false;
             this.loginBar.Location = new System.Drawing.Point(715, 72);
             this.loginBar.Name = "loginBar";
             this.loginBar.Size = new System.Drawing.Size(280, 1);
             this.loginBar.TabIndex = 3;
             // 
-            // textBox1
+            // BtnIngresarBar
             // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.Menu;
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.textBox1.Location = new System.Drawing.Point(36, 67);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(169, 19);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.BtnIngresarBar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnIngresarBar.Location = new System.Drawing.Point(174, 160);
+            this.BtnIngresarBar.Name = "BtnIngresarBar";
+            this.BtnIngresarBar.Size = new System.Drawing.Size(75, 23);
+            this.BtnIngresarBar.TabIndex = 2;
+            this.BtnIngresarBar.Text = "Ingresar";
+            this.BtnIngresarBar.UseVisualStyleBackColor = true;
             // 
-            // textBox2
+            // txtPAsswordBar
             // 
-            this.textBox2.BackColor = System.Drawing.SystemColors.MenuBar;
-            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.textBox2.Location = new System.Drawing.Point(36, 117);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(169, 19);
-            this.textBox2.TabIndex = 1;
-            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.textBox2.UseSystemPasswordChar = true;
+            this.txtPAsswordBar.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.txtPAsswordBar.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPAsswordBar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPAsswordBar.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtPAsswordBar.Location = new System.Drawing.Point(36, 117);
+            this.txtPAsswordBar.Name = "txtPAsswordBar";
+            this.txtPAsswordBar.Size = new System.Drawing.Size(169, 19);
+            this.txtPAsswordBar.TabIndex = 1;
+            this.txtPAsswordBar.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtPAsswordBar.UseSystemPasswordChar = true;
             // 
-            // button1
+            // txtUsuarioBar
             // 
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.Location = new System.Drawing.Point(174, 160);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.txtUsuarioBar.BackColor = System.Drawing.SystemColors.Menu;
+            this.txtUsuarioBar.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUsuarioBar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuarioBar.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtUsuarioBar.Location = new System.Drawing.Point(36, 67);
+            this.txtUsuarioBar.Name = "txtUsuarioBar";
+            this.txtUsuarioBar.Size = new System.Drawing.Size(169, 19);
+            this.txtUsuarioBar.TabIndex = 0;
+            this.txtUsuarioBar.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Principal
             // 
@@ -210,12 +210,12 @@
             this.Text = "Principal";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Principal_FormClosing);
             this.panel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.BtnCarrito)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.BtnUser)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.BtnMinimizar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.BtnCerrar)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnUser)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnCarrito)).EndInit();
             this.loginBar.ResumeLayout(false);
             this.loginBar.PerformLayout();
             this.ResumeLayout(false);
@@ -234,9 +234,9 @@
         private System.Windows.Forms.PictureBox BtnCarrito;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel loginBar;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtPAsswordBar;
+        private System.Windows.Forms.TextBox txtUsuarioBar;
+        private System.Windows.Forms.Button BtnIngresarBar;
 
     }
 }
