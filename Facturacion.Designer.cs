a3df5f23a5e4793d0f7059ed72f32af6b8fd817c
c3rberuss <aaamaya98@gmail.com>
2016-10-05 23:30:42 -0600

    formulario facturacion

--- Facturacion.Designer.cs
+++ Facturacion.Designer.cs
@@ -28,53 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
-            System.Windows.Forms.ListViewGroup listViewGroup1 = new System.Windows.Forms.ListViewGroup("ListViewGroup", System.Windows.Forms.HorizontalAlignment.Left);
-            this.PanelBarraTitulos = new System.Windows.Forms.Panel();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Bar = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
-            // PanelBarraTitulos
+            // Bar
             // 
-            this.PanelBarraTitulos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(58)))), ((int)(((byte)(69)))));
-            this.PanelBarraTitulos.Dock = System.Windows.Forms.DockStyle.Top;
-            this.PanelBarraTitulos.Location = new System.Drawing.Point(0, 0);
-            this.PanelBarraTitulos.Name = "PanelBarraTitulos";
-            this.PanelBarraTitulos.Size = new System.Drawing.Size(758, 30);
-            this.PanelBarraTitulos.TabIndex = 0;
-            // 
-            // listView1
-            // 
-            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader1});
-            this.listView1.GridLines = true;
-            listViewGroup1.Header = "ListViewGroup";
-            listViewGroup1.Name = "listViewGroup1";
-            listViewItem1.Group = listViewGroup1;
-            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem1});
-            this.listView1.Location = new System.Drawing.Point(70, 148);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(421, 97);
-            this.listView1.TabIndex = 1;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
-            // 
-            // columnHeader1
-            // 
-            this.columnHeader1.Width = 93;
+            this.Bar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(58)))), ((int)(((byte)(69)))));
+            this.Bar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Bar.Location = new System.Drawing.Point(0, 0);
+            this.Bar.Name = "Bar";
+            this.Bar.Size = new System.Drawing.Size(815, 30);
+            this.Bar.TabIndex = 0;
             // 
             // Facturacion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(758, 419);
-            this.Controls.Add(this.listView1);
-            this.Controls.Add(this.PanelBarraTitulos);
+            this.ClientSize = new System.Drawing.Size(815, 356);
+            this.Controls.Add(this.Bar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Facturacion";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Facturacion";
             this.ResumeLayout(false);
 
@@ -82,8 +56,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Panel PanelBarraTitulos;
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.Panel Bar;
     }
 }
